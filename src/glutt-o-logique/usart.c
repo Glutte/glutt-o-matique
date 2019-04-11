7c7cd71c56808610be6af41dbeb09637026414bb
Matthias P. Braendli <matthias.braendli@mpb.li>
2019-04-11 22:51:17 +0200

    Switch debug UART to 115200

--- src/glutt-o-logique/usart.c
+++ src/glutt-o-logique/usart.c
@@ -63,7 +63,7 @@ void usart_init() {
 
     // Setup USART2 for 9600,8,N,1
     USART_InitTypeDef USART_InitStruct;
-    USART_InitStruct.USART_BaudRate = 9600;
+    USART_InitStruct.USART_BaudRate = 115200;
     USART_InitStruct.USART_WordLength = USART_WordLength_8b;
     USART_InitStruct.USART_StopBits = USART_StopBits_1;
     USART_InitStruct.USART_Parity = USART_Parity_No;
